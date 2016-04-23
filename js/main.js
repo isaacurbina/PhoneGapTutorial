4b8f7941a9ff9c2d4bea6106266959c6d0806554
isaacurbina <isaac.urbina@live.com>
2016-04-23 12:09:36 -0400

    changed functionality a bit

--- js/main.js
+++ js/main.js
@@ -14,9 +14,28 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    },
+
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+},
 
 };
 
