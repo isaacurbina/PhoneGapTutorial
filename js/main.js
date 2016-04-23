fb76a12f191ebe1bc43c835176bb56a769681d29
isaacurbina <isaac.urbina@live.com>
2016-04-23 12:13:42 -0400

    fixed javascript

--- js/main.js
+++ js/main.js
@@ -24,18 +24,17 @@ var app = {
     },
 
     showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
     renderHomeView: function() {
         $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-},
+    }
 
 };
 
